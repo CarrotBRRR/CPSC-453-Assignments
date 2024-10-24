2e34beaa2b2a9db3d812b145a44a1363015cbbbb
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-10-23 22:32:26 -0600

    Finished A2

--- Assignment 2/assignment-2/main.cpp
+++ Assignment 2/assignment-2/main.cpp
@@ -15,91 +15,18 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
+#include <glm/gtc/type_ptr.hpp>
 
-// An example struct for Game Objects.
-// You are encouraged to customize this as you see fit.
-struct GameObject {
-	// Struct's constructor deals with the texture.
-	// Also sets default position, theta, scale, and transformationMatrix
-	GameObject(std::string texturePath, GLenum textureInterpolation) :
-		texture(texturePath, textureInterpolation),
-		position(0.0f, 0.0f, 0.0f),
-		theta(0),
-		scale(1),
-		transformationMatrix(1.0f) // This constructor sets it as the identity matrix
-	{}
-
-	CPU_Geometry cgeom;
-	GPU_Geometry ggeom;
-	Texture texture;
-
-	glm::vec3 position;
-	float theta; // Object's rotation
-	// Alternatively, you could represent rotation via a normalized heading vec:
-	// glm::vec3 heading;
-	float scale; // Or, alternatively, a glm::vec2 scale;
-	glm::mat4 transformationMatrix;
-};
-
-// EXAMPLE CALLBACKS
-class MyCallbacks : public CallbackInterface {
-
-public:
-	MyCallbacks(ShaderProgram& shader) : shader(shader) {}
-
-	virtual void keyCallback(int key, int scancode, int action, int mods) {
-		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-			shader.recompile();
-		}
-	}
-
-private:
-	ShaderProgram& shader;
-};
-
-CPU_Geometry shipGeom(float width, float height) {
-	float halfWidth = width / 2.0f;
-	float halfHeight = height / 2.0f;
-	CPU_Geometry retGeom;
-	// vertices for the spaceship quad
-	retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, halfHeight, 0.f));
-
-	// For full marks (Part IV), you'll need to use the following vertex coordinates instead.
-	// Then, you'd get the correct scale/translation/rotation by passing in uniforms into
-	// the vertex shader.
-	/*
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, 1.f, 0.f));
-	*/
-
-	// texture coordinates
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 1.f));
-	return retGeom;
-}
-
-// END EXAMPLES
+#include "GameObject.h"
+#include "Callbacks.h"
+#include "Game.h"
 
 int main() {
 	Log::debug("Starting main");
 
 	// WINDOW
 	glfwInit();
-	Window window(800, 800, "CPSC 453"); // can set callbacks at construction if desired
-
+	Window window(800, 800, "CPSC 453: Assignment 2"); // can set callbacks at construction if desired
 
 	GLDebug::enable();
 
@@ -107,38 +34,110 @@ int main() {
 	ShaderProgram shader("shaders/test.vert", "shaders/test.frag");
 
 	// CALLBACKS
-	window.setCallbacks(std::make_shared<MyCallbacks>(shader)); // can also update callbacks to new ones
+	std::shared_ptr<MyCallbacks> callback = std::make_shared<MyCallbacks>(shader);
+	window.setCallbacks(callback);
 
-	// GL_NEAREST looks a bit better for low-res pixel art than GL_LINEAR.
-	// But for most other cases, you'd want GL_LINEAR interpolation.
 	GameObject ship("textures/ship.png", GL_NEAREST);
+	GameObject diamond1("textures/diamond.png", GL_NEAREST, glm::vec3(((float) rand() / (RAND_MAX)) - 0.5f, ((float) rand() / (RAND_MAX)) - 0.5f, 0.f), glm::vec3(0.5f,0.25f,0.f));
+	GameObject diamond2("textures/diamond.png", GL_NEAREST, glm::vec3(((float) rand() / (RAND_MAX)) - 0.5f, ((float) rand() / (RAND_MAX)) - 0.5f, 0.f), glm::vec3(0.25f,0.5f,0.f));
+	GameObject diamond3("textures/diamond.png", GL_NEAREST, glm::vec3(((float) rand() / (RAND_MAX)) - 0.5f, ((float) rand() / (RAND_MAX)) - 0.5f, 0.f), glm::vec3(-0.5f,-0.5f,0.f));
+	GameObject diamond4("textures/diamond.png", GL_NEAREST, glm::vec3(((float) rand() / (RAND_MAX)) - 0.5f, ((float) rand() / (RAND_MAX)) - 0.5f, 0.f), glm::vec3(-0.15f,0.35f,0.f));
+	GameObject diamond5("textures/diamond.png", GL_NEAREST, glm::vec3(((float) rand() / (RAND_MAX)) - 0.5f, ((float) rand() / (RAND_MAX)) - 0.5f, 0.f), glm::vec3(0.5f,-0.5f,0.f));
 
-	ship.cgeom = shipGeom(0.18f, 0.12f);
+	std::vector<GameObject*> uncollected_dia_base = std::vector<GameObject*>();
+	uncollected_dia_base.push_back(&diamond1);
+	uncollected_dia_base.push_back(&diamond2);
+	uncollected_dia_base.push_back(&diamond3);
+	uncollected_dia_base.push_back(&diamond4);
+	uncollected_dia_base.push_back(&diamond5);
+	
+	std::vector<GameObject*> uncollected_dia = std::vector<GameObject*>();
+	uncollected_dia.push_back(&diamond1);
+	uncollected_dia.push_back(&diamond2);
+	uncollected_dia.push_back(&diamond3);
+	uncollected_dia.push_back(&diamond4);
+	uncollected_dia.push_back(&diamond5);
 
-
-	ship.ggeom.setVerts(ship.cgeom.verts);
-	ship.ggeom.setTexCoords(ship.cgeom.texCoords);
+	int score = 0;
+	bool start = false;
+	int maxScore = uncollected_dia.size();
 
 	// RENDER LOOP
 	while (!window.shouldClose()) {
-		int score;
+		
+		Params user_input = callback->getParams();
+
+		if (user_input.resetFlag){
+			resetGame(&ship, &uncollected_dia_base, &uncollected_dia);
+			score = 0;
+			start = false;
+		}
+
+		if (user_input.move_distance != glm::vec3(0.f,0.f,0.f)){
+			start = true;
+		}
+
 		glfwPollEvents();
 
 		shader.use();
-		ship.ggeom.bind();
 
+		// Clear the screen
 		glEnable(GL_FRAMEBUFFER_SRGB);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		std::vector<int> collectedIndices = std::vector<int>();
+		int newlyCollected = 0;
+		// Then render the diamonds
+		for (int i = 0; i < int(uncollected_dia.size()); i++){
+
+			auto curr = uncollected_dia.at(i);
+
+			(*curr).ggeom.bind();
+			(*curr).texture.bind();
+
+			(*curr).updateDiamondPos();
+
+			if (isDiamondCollected(&(ship.position), &((*curr).position)) && start){
+				newlyCollected += 1;
+				score += 1;
+				collectedIndices.push_back(i);
+			}
+			else{
+				(*curr).ggeom.bind();
+				(*curr).texture.bind();
+	
+				glm::mat4 M_diamond = (*curr).combinedMatrix();
+				glUniformMatrix4fv(0,1,GL_FALSE, glm::value_ptr(M_diamond));
+
+				glDrawArrays(GL_TRIANGLES, 0, 6);
+				(*curr).texture.unbind();
+			}
+		}
+
+		for (int i = 0; i < int(collectedIndices.size()); i++){
+			uncollected_dia.erase(std::remove(uncollected_dia.begin(), uncollected_dia.end(), uncollected_dia.at(collectedIndices.at(i))), uncollected_dia.end());
+		}
+
+		ship.updateShip(user_input, newlyCollected);
+		
+		//Render the ship
+		ship.ggeom.bind();
 		ship.texture.bind();
+
+		// Transformation
+		glm::mat4 M_ship = ship.combinedMatrix();
+		glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(M_ship));
+		
+		// Draw ship then unbind texture
 		glDrawArrays(GL_TRIANGLES, 0, 6);
+		
 		ship.texture.unbind();
+		
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 
-		// Starting the new ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		// Putting the text-containing window in the top-left of the screen.
 		ImGui::SetNextWindowPos(ImVec2(5, 5));
 
 		// Setting flags
@@ -155,19 +154,24 @@ int main() {
 		// Begin a new window with these flags. (bool *)0 is the "default" value for its argument.
 		ImGui::Begin("scoreText", (bool *)0, textWindowFlags);
 
-		// Scale up text a little, and set its value
-		ImGui::SetWindowFontScale(1.5f);
-		ImGui::Text("Score: %d", 0); // Second parameter gets passed into "%d"
-
-		// End the window.
+		
+		if (score == maxScore){
+			ImGui::SetWindowFontScale(3.0f);
+			ImGui::Text("You win!");
+		}
+		else{
+			ImGui::SetWindowFontScale(1.5f);
+			ImGui::Text("Score: %d", score); // Second parameter gets passed into "%d"
+		}
+		
 		ImGui::End();
 
-		ImGui::Render();	// Render the ImGui window
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); // Some middleware thing
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		window.swapBuffers();
 	}
-	// ImGui cleanup
+
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
